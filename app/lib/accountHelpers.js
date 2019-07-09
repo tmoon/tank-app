6652efcaeabbe6b11ff9aee2bab0efa4754ff1fb
Nazim <nazim.001.618@gmail.com>
2019-07-09 15:02:12 +0600

    accountHelpers and pinCryptoHelpers code completed

--- app/lib/accountHelpers.js
+++ app/lib/accountHelpers.js
@@ -1,11 +1,67 @@
 const terra = require('@terra-money/core');
+const encryption_helpers = require('./pinCryptoHelpers');
+const sha256 = require('js-sha256');
+const AsyncStorage = require("react-native").AsyncStorage;
 
-function create_new_user(pin) {
-
-    // returns pub key, encrypted mnemonic
-}
-
-function recover_user(mnemonic, pin) {
+// NOTE: pin must be string
+async function create_new_user(pin) {
+    const mnemonic = terra.generateMnemonic()
+    const masterKey = await terra.deriveMasterKey(mnemonic)
+    const keypair = terra.deriveKeypair(masterKey)
+    const accAddr = terra.getAccAddress(keypair.publicKey)
     
-    return encrypted_mnemonic, address
+    if(await encrypt_and_save(keypair, pin) == false) {
+        return {
+            error : true,
+            message: "Internal Encryption Error"
+        }
+    }
+
+    return { 
+                "mnemonic" : mnemonic,
+                "account_address" : accAddr
+            };
 }
+
+// NOTE: pin must be string
+async function recover_user(mnemonic, pin) {
+    const masterKey = await terra.deriveMasterKey(mnemonic);
+    const keypair = terra.deriveKeypair(masterKey);
+    const accAddr = terra.getAccAddress(keypair.publicKey);
+
+    if(await encrypt_and_save(keypair, pin) == false) {
+        return {
+            error : true,
+            message: "Internal Encryption Error"
+        }
+    }
+
+    return { 
+                "account_address" : accAddr
+            };
+}
+
+// NOTE: keypair[must be buffer pair], pin must be string
+async function encrypt_and_save(keypair, pin) {
+    try {
+            pin_hash = sha256.sha256(pin);
+            publicKey = encryption_helpers.encrypt(keypair.publicKey.toString('hex'), pin);
+            privateKey = encryption_helpers.encrypt(keypair.privateKey.toString('hex'), pin);
+            
+            await AsyncStorage.setItem("pin_hash", pin_hash);
+            await AsyncStorage.setItem("publicKey", publicKey);
+            await AsyncStorage.setItem("privateKey", privateKey);
+
+            // console.log(await AsyncStorage.getItem("pin_hash"), await AsyncStorage.getItem("publicKey"), await AsyncStorage.getItem("privateKey"));
+
+    } catch (error) {
+        return false;
+    }
+
+    return true;
+}
+
+module.exports = {
+    create_new_user,
+    recover_user
+};
--- app/lib/pinCryptoHelpers.js
+++ app/lib/pinCryptoHelpers.js
@@ -1,10 +1,49 @@
+// NOTE: pin must be a string. Return pin after fixing it length
+function fix_pin(pin) {
+    while(pin.length < 16) {
+        pin += pin.split("").reverse().join("");
+    }
+    return pin;
+}
+
+// NOTE: pin and string must be string. Return encrypted string
 function encrypt_with_pin(pin, string) {
-    
+    if(typeof pin !== "string") {
+        throw "Pin must be a string";
+    }
+
+    if(typeof string !== "string"){
+        throw "Message for encryption must be a string";
+    }
+
+    pin = fix_pin(pin);
+
+    const encryptor = require('simple-encryptor')(pin);
+    let encrypted_string = encryptor.encrypt(string);
+
     return encrypted_string
 }
 
+// NOTE: pin and string must be string. Return decrypted string
 function decrypt_with_pin(pin, string) {
-    
+    if(typeof pin !== "string") {
+        throw "Pin must be a string";
+    }
+
+    if(typeof string !== "string") {
+        throw "Message for decryption must be a string";
+    }
+
+    pin = fix_pin(pin);
+
+    const decryptor = require('simple-encryptor')(pin);
+    let decrypted_string = decryptor.decrypt(string);
+
     return decrypted_string
 }
+
+module.exports = {
+    encrypt : encrypt_with_pin,
+    decrypt : decrypt_with_pin
+};
 
